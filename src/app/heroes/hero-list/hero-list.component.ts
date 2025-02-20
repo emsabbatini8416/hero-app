5122e274ae0a10d33231961b6d0c1085fcbbdcd1
emsabbatini8416 <emmanuel.sabbatini@gmail.com>
2025-02-20 16:47:50 -0500

    added heroes filter

--- src/app/heroes/hero-list/hero-list.component.ts
+++ src/app/heroes/hero-list/hero-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { HeroService } from '../../services/hero.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -12,12 +12,24 @@ import { RouterModule } from '@angular/router';
 })
 export class HeroListComponent {
   heroes;
+  filterText = signal<string>('')
 
   constructor(private heroService: HeroService) {
     this.heroes = this.heroService.heroes;
   }
 
+  filteredHeroes = computed(() =>
+    this.heroes().filter((hero) =>
+      hero.name.toLowerCase().includes(this.filterText().toLowerCase())
+    )
+  );
+
   deleteHero(id: number) {
     this.heroService.deleteHero(id);
+  }
+
+  onFilterChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.filterText.set(value);
   }
 }
