410d1dfd0a56501864f56d559fb09ad06cbe67bf
emsabbatini8416 <emmanuel.sabbatini@gmail.com>
2025-02-24 20:48:13 -0500

    added new decorators for if and for

--- src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
+++ src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
@@ -1,22 +1,20 @@
-import { Component, inject } from '@angular/core';
-import { ModalService } from '../../../services/modal.service';
-import { NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core'
+import { ModalService } from '../../../services/modal.service'
 
 @Component({
   standalone: true,
   selector: 'app-confirmation-modal',
-  imports: [NgIf],
   templateUrl: './confirmation-modal.component.html',
   styleUrls: ['./confirmation-modal.component.scss'],
 })
 export class ConfirmationModalComponent {
-  modalService = inject(ModalService);
+  modalService = inject(ModalService)
 
   confirm() {
-    this.modalService.confirm();
+    this.modalService.confirm()
   }
 
   cancel() {
-    this.modalService.cancel();
+    this.modalService.cancel()
   }
 }
